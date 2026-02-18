efa12d82f44991df404a31652e1988bfeb2b47d4
Roysteroonie <roy.knowles@me.com>
2026-02-18 16:24:16 +0000

    Update hex.go

--- cmd/hex.go
+++ cmd/hex.go
@@ -18,7 +18,7 @@ package cmd
 
 import (
 	"encoding/hex"
-	"github.com/buxtronix/phev2mqtt/protocol"
+	"github.com/buxtronix/Roysteroonie/protocol"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
