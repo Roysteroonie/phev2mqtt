47a20bc621d93d27ff898d2f68c61baf1a1eea43
Roysteroonie <roy.knowles@me.com>
2026-02-18 16:25:14 +0000

    Update pcap.go

--- cmd/pcap.go
+++ cmd/pcap.go
@@ -26,7 +26,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/buxtronix/phev2mqtt/protocol"
+	"github.com/Roysteroonie/phev2mqtt/protocol"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
